aa3d5563f331b12ad8247fad0c6120d14402239f
1usdapp <hhveryquickly@gmail.com>
2025-11-13 00:17:23 +0800

    feature: protobuf 2 json

--- src/con_mgr.cpp
+++ src/con_mgr.cpp
@@ -1,7 +1,47 @@
 #include "con_mgr.hpp"
 #include "google/protobuf/util/json_util.h"
-
+#include <type_traits>
 namespace lps {
+
+// SFINAE 检测器
+template<typename T, typename = void>
+struct has_always_print_primitive_fields : std::false_type {};
+
+template<typename T>
+struct has_always_print_primitive_fields<T, 
+    std::void_t<decltype(std::declval<T>().always_print_primitive_fields)>> 
+    : std::true_type {};
+
+template<typename T, typename = void>
+struct has_always_print_fields_with_no_presence : std::false_type {};
+
+template<typename T>
+struct has_always_print_fields_with_no_presence<T,
+    std::void_t<decltype(std::declval<T>().always_print_fields_with_no_presence)>>
+    : std::true_type {};
+
+// 使用函数重载而不是 if constexpr
+template<typename OptionsType>
+void SetOptionalField(OptionsType& options, std::true_type /* has_always_print_primitive_fields */) {
+    options.always_print_primitive_fields = true;
+}
+
+template<typename OptionsType>
+void SetOptionalField(OptionsType& options, std::false_type /* has_always_print_primitive_fields */) {
+    // 不设置该字段
+    (void)options;
+}
+
+template<typename OptionsType>
+void SetOptionalField2(OptionsType& options, std::true_type /* has_always_print_fields_with_no_presence */) {
+    options.always_print_fields_with_no_presence = true;
+}
+
+template<typename OptionsType>
+void SetOptionalField2(OptionsType& options, std::false_type /* has_always_print_fields_with_no_presence */) {
+    // 不设置该字段
+    (void)options;
+}
 
 uint32_t CConMgr::on_get_conid()
 {
@@ -64,7 +104,10 @@ std::string CConMgr::client_info()
     options.add_whitespace = true;                   // 添加缩进和换行
     options.preserve_proto_field_names = true;       // 保持原始字段名（默认转为驼峰）
     options.always_print_enums_as_ints = true;       // 枚举是否总是输出为整数
-    options.always_print_primitive_fields = true;   // 是否打印默认值
+
+    // 使用 SFINAE 函数重载
+    SetOptionalField(options, has_always_print_primitive_fields<google::protobuf::util::JsonPrintOptions>{});
+    SetOptionalField2(options, has_always_print_fields_with_no_presence<google::protobuf::util::JsonPrintOptions>{});
 
     auto status =
         google::protobuf::util::MessageToJsonString(cache_clients_info, &json_string, options);
